dceae051010bd12d23580d3d62256cfa869bbdeb
G00374265@atu.ie <Michael.e.agbo.work@gmail.com>
2023-11-08 10:36:06 +0000

    Exam

--- src/main/java/ie/atu/WarehouseServiceClient.java
+++ src/main/java/ie/atu/WarehouseServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Warehouse Service", url = "https:/")
+@FeignClient(name = "Warehouse Service", url = "https:/localhost:8081")
 public interface WarehouseServiceClient {
     @PostMapping("/warehouse")
     String someDetails(@RequestBody ProductDetails productDetails);
